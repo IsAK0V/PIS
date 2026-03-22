152f310edaf2b2be959b69ee3012fe40f4ab9b32
IsAK0V <48669333+IsAK0V@users.noreply.github.com>
2026-03-22 21:44:11 +0300

    Delete Lab 1- 4 directory
    изменение данных

--- Lab 1- 4/lab3/blog/articles/views.py
+++
@@ -1,6 +0,0 @@
-# Create your views here.
-from models import Article
-from django.shortcuts import render
-def archive(request):
- return render(request, 'archive.html', {"posts":
-Article.objects.all()})
